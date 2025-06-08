99d68fd4bc6072a44c05948813809856491c6d33
gggentooo <gggentooo@gmail.com>
2025-06-09 08:38:04 +0900

    Edited header and footer to be injected by js:

--- src/js/common.js
+++ src/js/common.js
@@ -10,10 +10,22 @@ function toggleDark() {
     localStorage.setItem('darkMode', isDark ? 'enabled' : 'disabled');
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.getItem('darkMode') === 'enabled') {
         document.documentElement.classList.add('dark');
     }
+
+    document.getElementsByTagName('header')[0].innerHTML = `
+        <span class="pagetitle" onclick="location.href='https://lnaba.reisen/'">lnaba.reisen</span>
+        <button class="darkmode" onclick="toggleDark()">Toggle Dark Mode</button>
+        <nav>
+            <a href="https://lnaba.reisen/">Main</a>
+            <a href="https://lnaba.reisen/now">Now</a>
+            <a href="https://lnaba.reisen/thoughts">Thoughts</a>
+            <a href="https://lnaba.reisen/things">Things</a>
+        </nav>`;
+        
+    document.getElementsByTagName('footer')[0].innerHTML = `<a href="mailto:d@lnaba.reisen">Made and Written by D</a>`;
 });
 
 function langOpenClose(target_lang) {
@@ -22,5 +34,3 @@ function langOpenClose(target_lang) {
         target_elems[i].classList.toggle("hide");
     }
 }
-
-
